03da8978ac6a1842c58d6f7d488231afcf0c8aab
jsemple19 <semple.j@gmail.com>
2020-03-30 23:45:58 +0200

    all R distributions

--- Chapter01_exercises.R
+++ Chapter01_exercises.R
@@ -80,6 +80,8 @@ extremeValueProb(m=8,lambda=7)
 lambda=0.1 # false positive rate P(declare epitope | no epitope)
 n=100 # number of positions in protein that are tested (assumed independant)
 m=9 #  threshold number for false positives not tolerated
+
+# What is the probability of getting more than 9 false positives in this ELISA test?
 # to show that the probability is smaller than 0.000001 (1e-6) we would
 # need at least 1e7 simulations
 maxes<-replicate(1e7,expr={max(rpois(n=n,lambda=lambda))})
@@ -87,3 +89,84 @@ mean(maxes>=m)
 # lets check with our function from before
 extremeValueProb(m=m,n=n,lambda=lambda)
 
+
+## Exercise 1.6
+?Distributions
+xInteger<-c(1:100)
+xCont<-seq(1,50,0.5)
+xProb<-seq(0,1,0.01)
+
+# beta (continuous)
+# distribution of two probabilites
+plot(xProb,dbeta(xProb,shape1=0.3,shape2=0.8),main="beta")
+
+# binomial (discrete)
+# number of successes in bernoulli trials
+plot(xInteger,dbinom(xInteger,size=100,prob=0.3),main="binomial")
+#plot(xCont,dbinom(xCont,size=100,prob=0.3))
+
+# Cauchy (continuous)
+# describes the ratio of two independant normal distributions with mean 0
+plot(xCont,dcauchy(xCont,location=mean(xInteger)),main="Cauchy")
+
+# Chi squared (continuous)
+# special case of gamma distribution
+# sum of squares of k independant standard normal random variables
+plot(xCont,dchisq(xCont,df=3),main="Chi squared")
+
+# exponential (continuous)
+# time between events in a Poisson proces (i.e events occur continuously
+# and independently at a constant average rate)
+# this is the continous analog of geometric distribution
+plot(xCont,dexp(xCont,rate=0.3),main="exponential")
+
+# F distribution (continuous)
+# arises frequently as the null distribution of a test statistic
+# (esecially ANOVA). It is the ratio of the mean squares of two
+# independant standard normally distributed variables
+plot(xCont,df(xCont,df1=3,df2=5),main="F distribution")
+
+# geometric (discrete)
+# the probability that the first occurrence of success requires k
+# independent trials, each with success probability p.
+plot(xInteger,dgeom(xInteger,prob=0.02),main="geometric")
+
+# hypergeometric (discrete)
+# the probability of x sucesses in k draws WITHOUT replacement from
+# a finite population size m+n that contains m objects with that feature.
+# NOTE: binomial described probability o k success in n draws WITH replacement
+plot(xInteger,dhyper(xInteger,m=20,n=80,k=20),main="hypergeometric")
+
+# log-normal (continuous)
+# distribution of a random variable whose logarithm is normally distributed
+plot(xCont,dlnorm(xCont,meanlog=mean(log(xCont))),main="log-normal")
+
+# multinomial (discrete)
+# probability of counts for each of a k-sided die rolled n times.
+# no meaningful easy plot
+
+# negative bionomial (discrete)
+# the number of failures in a sequence of bernouli trials before a
+# specified number of successes occurs.
+plot(xInteger,dnbinom(xInteger,size=10,prob=c(0.3)),main="negative binomial")
+
+# normal (continuous)
+# distribution of a real-valued random variable
+plot(xCont,dnorm(xCont,mean=mean(xCont)))
+
+# poisson (discrete)
+# probability of a given number of events happening in a fixed
+# interval of time or space
+plot(xInteger,dpois(xInteger, lambda=10),main="lambda")
+
+# t (continuous)
+# distribution of estimates of the man of a normally distributed
+# population with a samll sample size.
+plot(xCont,dt(xCont,df=5,ncp=mean(xCont)),main="t distribution")
+
+# uniform (continuous)
+plot(xCont,dunif(xCont,min=min(xCont),max=max(xCont)),"uniform")
+
+# Weibull (continuous)
+# describes particle size distribution
+plot(xCont,dweibull(xCont,shape=10,scale=mean(xCont)))
