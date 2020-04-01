405fa0c6730f9a02a139324cbd5ed4efde5dce2a
jsemple19 <semple.j@gmail.com>
2020-04-01 11:12:34 +0200

    starting chapter 2

---
+++ chapter02/Chapter02.R
@@ -0,0 +1,8 @@
+##############################################################
+####  CHAPTER 2 - statistical modelling                #######
+##############################################################
+# exercises in text
+
+# In the previous chapter we had clear theoretical models of the data, so that
+# given a distribution and its parameters, we could calculate probability
+# In this chapter we work upwards from the data
